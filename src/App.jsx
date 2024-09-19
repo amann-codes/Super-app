0b2af56c299962fa1e9ccce97d620ce2fd1f8ba1
amanprajapati00 <aman.prajapati3000@gmail.com>
2024-09-19 22:06:37 +0530

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Registration from "./pages/Registration";
+import Category from "./pages/category";
 export default function App(){
   return (
     <div>
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Registration/>}/>
+        <Route path="/category" element={<Category/>}/>
       </Routes>
       </BrowserRouter>
     </div>
