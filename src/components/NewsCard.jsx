cde1177c96cf12d6863a8999630faaf872875f22
amanprajapati00 <aman.prajapati3000@gmail.com>
2024-09-24 18:34:37 +0530

    added news card”

---
+++ src/components/NewsCard.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const NewsCard = () => {
+  const [article, setArticle] = useState({
+    picture: "",
+    time: "",
+    content: "",
+    title: "",
+  });
+  const apiKey = import.meta.env.VITE_API_NEWS_KEY;
+
+  useEffect(() => {
+    async function fetchNews() {
+      try {
+        const response = await axios.get(
+          `https://newsapi.org/v2/everything?q=tesla&from=2024-08-24&sortBy=publishedAt&apiKey=${apiKey}`
+        );
+        const article = response.data.articles[2];
+        setArticle({
+          picture: article.urlToImage,
+          time: article.publishedAt,
+          content: article.content,
+          title: article.title,
+        });
+      } catch (error) {
+        console.error("Error fetching news:", error);
+      }
+    }
+    fetchNews();
+  }, [apiKey]);
+
+  return (
+    <div className="flex flex-col w-[250px] h-[438px] rounded-xl bg-white overflow-hidden">
+      <div className="relative h-[300px]">
+        <img src={article.picture} className="w-full h-full object-cover" alt="News Article" />
+        <div className="absolute bottom-0 bg-black bg-opacity-75 flex flex-col justify-center px-3 py-2">
+          <h2 className="text-white text-xl font-bold mb-1">{article.title}</h2>
+          <p className="text-white text-sm">
+            {new Date(article.time).toLocaleDateString()}
+          </p>
+        </div>
+      </div>
+      <div className="p-3">
+        <p className="text-black text-sm line-clamp-5">{article.content}</p>
+      </div>
+    </div>
+  );
+};
+
+export default NewsCard;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import ProfileCard from '../components/ProfileCard'
-import WeatherCard from '../components/WeatherCard'
+import React from "react";
+import ProfileCard from "../components/ProfileCard";
+import WeatherCard from "../components/WeatherCard";
+import NewsCard from "../components/NewsCard";
 
 export default function Homepage() {
   return (
-    <div className='w-screen h-screen bg-black p-5'>
-      <div className='flex flex-col gap-3'>
-      <ProfileCard/>
-      <WeatherCard/>
+    <div className="w-screen h-screen bg-black p-5">
+      <div className="flex flex-row gap-3">
+        <div className="flex flex-col gap-3">
+          <ProfileCard />
+          <WeatherCard />
+        </div>
+        <NewsCard/>
       </div>
     </div>
-  )
+  );
 }
